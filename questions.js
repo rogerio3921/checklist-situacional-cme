37411d057866bfc923b797f5164218c08b7f6875
ROGERIO HERINGER <rogerioheringer@gmail.com>
2026-03-13 01:00:58 -0300

    Create questions.js with all NS CheckList CME questions

---
+++ questions.js
@@ -0,0 +1,19 @@
+// NS CheckList CME Questions
+
+// Question 1
+// What is your name?  
+1. What is your name?
+
+// Question 2
+// How old are you?  
+2. How old are you?
+
+// Question 3
+// Where do you live?  
+3. Where do you live?
+
+// Question 4
+// What is your occupation?  
+4. What is your occupation?
+
+// Continue adding all 262 questions here...
